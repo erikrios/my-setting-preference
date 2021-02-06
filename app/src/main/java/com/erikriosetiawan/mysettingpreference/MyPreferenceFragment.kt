1fd711e1da5e21b6c45576a885795486edeba294
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-06 19:23:18 +0700

    Create MyPreferenceFragment.kt

---
+++ app/src/main/java/com/erikriosetiawan/mysettingpreference/MyPreferenceFragment.kt
@@ -0,0 +1,10 @@
+package com.erikriosetiawan.mysettingpreference
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class MyPreferenceFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+}
